5ad16e9bfdbfb819ee332ac22e0979e9a7098afd
Caio Prado de Souza <cpssouza7@gmail.com>
2024-03-14 21:56:24 -0300

    Projeto CEP em JSON

---
+++ main.js
@@ -0,0 +1,13 @@
+function buscarCep(){
+    let input = document.getElementById('cep').value;
+
+
+    const ajax = new XMLHttpRequest();
+    ajax.open('GET', 'https://viacep.com.br/ws/'+input+'/json');
+    ajax.send();
+
+    ajax.onload = function(){
+        document.getElementById('texto1').innerHTML = this.responseText;
+
+}
+}
